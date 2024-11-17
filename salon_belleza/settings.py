ec70a6a067be82640440a31b449df5611450e3a7
lauths12 <l.carretero@uniandes.edu.co>
2024-11-17 16:14:21 -0500

    dios dime que ya otr vez

--- salon_belleza/settings.py
+++ salon_belleza/settings.py
@@ -124,7 +124,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",  # Solo si colocaste la carpeta en la raíz
 ]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
